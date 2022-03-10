54cd6b0423991cf5f97febede67acbacdf5c4810
AlexeyLunyakov <a.lunyakov@my.com>
2022-03-10 13:16:31 +0300

    Фиксит sign-up

--- interiorselection/accounts/views.py
+++ interiorselection/accounts/views.py
@@ -6,4 +6,4 @@ from django.views.generic import CreateView
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'signup.html'
+    template_name = 'registration/signup.html'
